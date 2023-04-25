fd14c38f31f289dc0c80d98ab92db39e7e3c79b2
princekumarpaswan <prasantandal123@gmail.com>
2023-04-25 21:05:23 +0530

    for loop

--- Wile.java
+++ Wile.java
@@ -1,9 +1,16 @@
 public class Wile {
     public static void main(String[] args) {
+
+        // While loop
         int count = 1;
         while (count != 5) {
             System.out.println("Value of count" + count);
             count++;
+        }
+
+        // for loop
+        for (int cot = 1; cot != 5; cot++) {
+            System.out.println("For loop" + cot);
         }
     }
 }
