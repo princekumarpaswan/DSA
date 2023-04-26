c6e5c1bf42311265ff6a20235ce3fd1e59867c8a
princekumarpaswan <prasantandal123@gmail.com>
2023-04-26 18:13:24 +0530

    added new file

---
+++ Even.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Write a program weather a number is even or not
+
+public class Even {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a number to be check for even and odd");
+
+        int num = in.nextInt();
+        if ((num % 2) == 0) {
+            System.out.println("The given number is even " + num);
+        } else {
+            System.out.println("The given number is odd " + num);
+        }
+
+    }
+}
