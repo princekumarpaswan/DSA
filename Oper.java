e12043bc7c98d67ef07de0ebc3481b3a1e6050aa
princekumarpaswan <prasantandal123@gmail.com>
2023-04-27 13:20:15 +0530

    solved a program operater

---
+++ Oper.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Oper {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int a = in.nextInt();
+        System.out.println("Enter Second number");
+        int b = in.nextInt();
+        System.out.println("Enter a operater (+, -, *, /)");
+        char op = in.next().charAt(0);
+
+        if (op == '+') {
+            System.out.println("The sum " + (a + b));
+        }
+        if (op == '-') {
+            System.out.println("The sub " + (a - b));
+        }
+        if (op == '*') {
+            System.out.println("The multiple " + (a * b));
+        }
+        if (op == '/') {
+            System.out.println("The division " + (a / b));
+        }
+        if (op != '+' && op != '-' && op != '*' && op != '/') {
+            System.out.println("You have given wrong input");
+        }
+
+        in.close();
+    }
+
+}
